5591ef199a257a1d86bc9e8c25e0015e906d9518
pavanjvm <pavanprasad05062gmail.com>
2025-02-26 22:00:00 +0530

    m

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,9 +2,15 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    const conn = await mongoose.connect(process.env.DOCUMENTDB_URI, {
+      ssl: true,
+      sslValidate: false, // Set to true if using a valid certificate
+      tlsCAFile: "global-bundle.pem", // Path to the CA certificate file
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`Amazon DocumentDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log("MongoDB connection error:", error);
+    console.error("Amazon DocumentDB connection error:", error);
   }
 };
