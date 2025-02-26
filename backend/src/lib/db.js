ea9686d2dc3ff8ac0000c2f6d69b5c96da9636ba
pavanjvm <pavanprasad05062gmail.com>
2025-02-26 22:05:51 +0530

    p

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -6,13 +6,15 @@ dotenv.config();
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.DOCUMENTDB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
       tls: true, // Use TLS instead of SSL
-      tlsCAFile: "global-bundle.pem", // Amazon CA Cert
+      tlsCAFile: "global-bundle.pem", // Correct way to specify the CA file
     });
 
     console.log(`Connected to Amazon DocumentDB: ${conn.connection.host}`);
   } catch (error) {
     console.error("DocumentDB connection error:", error);
-    process.exit(1);
+    process.exit(1); // Exit process if connection fails
   }
 };
