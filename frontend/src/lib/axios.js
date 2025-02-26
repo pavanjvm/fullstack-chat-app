3cd69ce9fdd59feec3ce6bf8fd30d61e9cdcfbea
pavanjvm <pavanprasad05062gmail.com>
2025-02-26 23:16:43 +0530

    m

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://54.80.46.133:5001/api" : "/api",
   withCredentials: true,
 });
